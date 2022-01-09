892de96a56ca648f23141a9caf33cc85561ffb27
unknown <you@example.com>
2022-01-09 16:45:58 +0600

    wolmart

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+    'use strict';
+    // Banner slider start
+    $('#banner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 4000,
+        nextArrow: '<i class="fas fa-chevron-right nxt_arr"></i>',
+        prevArrow: '<i class="fas fa-chevron-left prv_arr"></i>',
+      });
+
+
+});
